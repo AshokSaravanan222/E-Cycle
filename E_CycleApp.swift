d6a85b28f4c5de77f63e7de44ffe9f8c21388050
Ashok Saravanan <ashko.saravanan@gmail.com>
2023-11-29 09:33:08 -0500

    added firebase

--- E_CycleApp.swift
+++ E_CycleApp.swift
@@ -6,10 +6,21 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct E_CycleApp: App {
     
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var sheetManager = SheetManager()
     
     var body: some Scene {
